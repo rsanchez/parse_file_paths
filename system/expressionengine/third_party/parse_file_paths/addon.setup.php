2cd1827826a5489e5f19d0e17477d4d1edadd17c
Unknown <roger.hughes@triad.uk.com>
2019-02-01 10:27:22 +0000

    make it work with newer EE versions

---
+++ system/expressionengine/third_party/parse_file_paths/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author' => 'Rob Sanchez',
+	'author_url' => 'http://github.com/rsanchez',
+	'description' => 'Parses {filedir_X} variables',
+	'name' => 'Parse File Paths',
+	'namespace' => 'parse_file_paths',
+	'settings_exist' => FALSE,
+	'version' => '1.1.0',
+);
